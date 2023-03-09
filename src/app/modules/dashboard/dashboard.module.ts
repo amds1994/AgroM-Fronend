2bd56375c89b91f2dc42fa9207b62df3f669fe11
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-03-09 12:42:57 -0500

    Sidenav

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard.component';
 import { HomeComponent } from './componetns/home/home.component';
-import { RouterModule } from '@angular/router';
+// import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -12,7 +13,7 @@ import { RouterModule } from '@angular/router';
     HomeComponent],
   imports: [
     CommonModule,
-    RouterModule
+    SharedModule
   ]
 })
 export class DashboardModule { }
