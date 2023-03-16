3d0d49b0d30e104f7fc4f3b552c4d4d4ef952a5b
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-03-16 18:50:38 -0500

    Funcion deliminar y componente confirm

--- src/app/modules/shared/services/category.service.ts
+++ src/app/modules/shared/services/category.service.ts
@@ -40,4 +40,13 @@ export class CategoryService {
     return this.http.put(endpoint, body);
     
   }
+
+  /**
+   * Delete categories
+   */
+  deleteCategories(id: string) {
+      
+      const endpoint = `${base_url}/categories/${id}`;
+      return this.http.delete(endpoint);
+  }
 }
